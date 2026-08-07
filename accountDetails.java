042a576540f52ac191227e78cdcd13dcaa06cd5b
Oluwafemi Balogun <fembal72@yahoo.com>
2026-08-07 20:33:34 +0100

    Second Commit

---
+++ accountDetails.java
@@ -0,0 +1,11 @@
+public class AccountDetails {
+
+	doughs = 2000
+	serviette = 2000
+	cocktails = 1000
+	sum = doughs + serviette + coctails
+
+	public static void main(String[] args) {
+		System.out.println("The balance is " + sum + "$ in total.");
+	}
+}
---
+++ stockCalc.java
@@ -0,0 +1,13 @@
+public class StockCalc {
+	
+
+	opening stock = 6000
+	closing stock = 1470
+	available stock = closing stock - opening stock
+
+
+    public static void main(String[] args) {
+        System.out.println("The available stock from which funds can be 
+			drawn is " + available stock + "$" as at + date ".");
+    }
+}
